ab06ef9cdda1ce3c9c9e8a55c7b4ac5a4ffad5af
= <=>
2022-03-20 20:27:42 +0700

    first amend

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+class Singleton{
+    private:
+        Singleton();
+        static Singleton* _mObject;
+    public:
+        static Singleton* createInstance();
+        Singleton(Singleton&other) = delete;
+        void operator=(Singleton&other) = delete;
+
+};
+Singleton::Singleton()
+{
+    std::cout<<"Instantiate object\n";
+}
+Singleton* Singleton::_mObject = nullptr;
+Singleton* Singleton::createInstance()
+{
+    if (!_mObject)
+    {
+        _mObject = new Singleton();
+    }
+    return _mObject;
+}
+
+int main()
+{
+    Singleton* db;
+    db = Singleton::createInstance(); 
+}
